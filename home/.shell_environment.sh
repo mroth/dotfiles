aae2184681b3098f04277d3f57d45eaadad2c712
Matthew Rothenberg <mrothenberg@gmail.com>
2013-04-20 15:16:55 -0400

    set some convenience variables for things I can never remember where they are

--- home/.shell_environment.sh
+++ home/.shell_environment.sh
@@ -8,3 +8,9 @@ export CLICOLOR=1
 # add local repo binstubs to default path
 #
 export PATH=.bundle/binstubs:$PATH
+
+#
+# set some convenience variables for things I can never remember where they are
+#
+export ST2CONFIG="$HOME/Library/Application Support/Sublime Text 2/Packages/User"
+export DOTFILES="$HOME/.homesick/repos/dotfiles/home"
