f03b77b2a16f125fda7725d726488ba6f5726f09
Matthew Rothenberg <mrothenberg@gmail.com>
2013-04-28 15:12:38 -0400

    recursive touch

--- home/.shell_functions.sh
+++ home/.shell_functions.sh
@@ -1,3 +1,14 @@
+#
+# recursively touch, e.g. touch + mkdir -p
+# so files can easily be created at depth
+#
+canhaz () {
+  mkdir -p -- "${1%/*}" && touch -- "$1"
+}
+
+#
+# convenience functions for updating my shit
+#
 config_pull () {
   #TODO: pull dotfiles
   #pull sublime config
