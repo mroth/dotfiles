d4cfa57c73fc21f6d4555ab62f0b6921511de5c8
Matthew Rothenberg <mrothenberg@gmail.com>
2014-04-16 20:11:16 -0400

    add lolcommits delay since newer macbook pro seems to need it

--- home/.shell_environment.sh
+++ home/.shell_environment.sh
@@ -8,6 +8,11 @@ export CLICOLOR=1
 export GOPATH=$HOME/Dropbox/src/gocode
 
 #
+# lolcommits related prefs
+#
+export LOLCOMMITS_DELAY=1
+
+#
 # add local bin to default path
 # (in theory this should be in default path on Mavericks, not sure why not on mbair?)
 #
