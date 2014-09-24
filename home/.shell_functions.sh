ff1af8aec5de04ead18e4abea904511565eb4a51
Matthew Rothenberg <mrothenberg@gmail.com>
2014-09-24 15:28:26 -0400

    never finished/used this local pull thing

--- home/.shell_functions.sh
+++ home/.shell_functions.sh
@@ -5,18 +5,3 @@
 canhaz () {
   mkdir -p -- "${1%/*}" && touch -- "$1"
 }
-
-#
-# convenience functions for updating my shit
-#
-config_pull () {
-  #TODO: pull dotfiles
-  #pull sublime config
-  config_sublime_pull
-}
-
-config_sublime_pull () {
-  cd $ST2CONFIG
-  git pull
-  cd -
-}
