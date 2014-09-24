0b18750dbb92c45f22905febcc72460ff642b1f4
Matthew Rothenberg <mrothenberg@gmail.com>
2014-09-24 15:44:35 -0400

    clean ghc alias

--- home/.shell_aliases.sh
+++ home/.shell_aliases.sh
@@ -7,3 +7,8 @@ alias cdf='cd `osascript -e "tell application \"Finder\" to if window 1 exists t
 
 #homeshick as a pseudo command
 alias homesick=homeshick
+
+# "clean" GHC shoving intermediate files into /tmp
+# for when I'm working on quick scripts and dont really care about them
+# also enable -O2 optimization by default
+alias ghcc='ghc -O2 -outputdir /tmp/ghc'
