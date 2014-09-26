1498fdddf13ba7c54f4eeab80d6020b10e75e91f
Matthew Rothenberg <mrothenberg@gmail.com>
2014-09-26 15:53:17 -0400

    td todo alias

--- home/.shell_aliases.sh
+++ home/.shell_aliases.sh
@@ -1,8 +1,11 @@
 # list dotfiles
 alias lsd='ls -od \.*'
 
-#homeshick as a pseudo command
+# homeshick as a pseudo command
 alias homesick=homeshick
+
+# my code todos
+alias td='ack TODO'
 
 # "clean" GHC shoving intermediate files into /tmp
 # for when I'm working on quick scripts and dont really care about them
