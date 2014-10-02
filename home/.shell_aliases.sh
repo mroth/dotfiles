f91d16eb8677b97a19625643427aa9ea1a6b050b
Matthew Rothenberg <mrothenberg@gmail.com>
2014-10-02 18:33:21 -0400

    i like to check homebrew a lot

--- home/.shell_aliases.sh
+++ home/.shell_aliases.sh
@@ -7,6 +7,10 @@ alias homesick=homeshick
 # my code todos
 alias td='ack TODO'
 
+# for obsessive version checking
+alias brewsup='brew update && brew outdated'
+alias brewup='brew upgrade && brew cleanup'
+
 # "clean" GHC shoving intermediate files into /tmp
 # for when I'm working on quick scripts and dont really care about them
 # also enable -O2 optimization by default
