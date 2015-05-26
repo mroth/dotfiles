0ee290ea2d8035ba7b841922fa28226a5300da61
Matthew Rothenberg <mrothenberg@gmail.com>
2015-05-26 17:17:57 -0400

    update khan paths
    hopefully soon i will have changed enough with the actual dotfiles setup
    to migrate over, but for now, update the paths on my hacked setup to
    reflect my state of changes.

--- home/.shell_functions.sh
+++ home/.shell_functions.sh
@@ -14,14 +14,13 @@ khanify () {
   # override standard Git user info
   export GIT_AUTHOR_EMAIL=mroth@khanacademy.org
 
-  # Add ka-lint to PATH
-  export PATH="$HOME/src/khan/devtools/khan-linter/bin:$PATH"
-  # Add arcanist bins to PATH
-  export PATH="$HOME/src/khan/devtools/arcanist/khan-bin:$PATH"
-  # Add git-bigfiles to PATH
-  export PATH="$HOME/src/khan/devtools/git-bigfile/bin:$PATH"
   # Add frankenserver bins to PATH
   export PATH="$HOME/src/khan/webapp/third_party/frankenserver:$PATH"
-  # Add dotfiles scripts to PATH
-  export PATH="$HOME/src/khan/khan-dotfiles/bin:$PATH"
+
+  # Add devtools bins to PATH
+  KA_DEVROOT="$HOME/src/khan/devtools"
+  export PATH="$KA_DEVROOT/arcanist/khan-bin:$PATH"
+  export PATH="$KA_DEVROOT/git-bigfile/bin:$PATH"
+  export PATH="$KA_DEVROOT/git-workflow/bin:$PATH"
+  export PATH="$KA_DEVROOT/khan-linter/bin:$PATH"
 }
