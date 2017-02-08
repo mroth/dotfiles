7d473dda607496ba47e77f438ec8c5749f1e5ee2
Matthew Rothenberg <mrothenberg@gmail.com>
2017-02-07 19:20:00 -0500

    just set a reasonable BUNDLE_JOBS default
    remove a binary execution every time a shell spawns, saving about 10ms
    
    the original was clever here, but I just don’t bundle install enough to
    need it micro-optimized

--- home/.shell_environment.sh
+++ home/.shell_environment.sh
@@ -27,19 +27,6 @@ export LOLCOMMITS_DELAY=1
 export PATH=.bundle/binstubs:$PATH
 
 #
-# Bundler: configure BUNDLE_JOBS for concurrent installs.
-# for details on why I set this the way I do, see:
-# http://blog.mroth.info/blog/2014/10/02/rubygems-bundler-they-took-our-jobs/
-#
-if [[ "$OSTYPE" = darwin* ]]
-then
-  local target_jobs="$(expr `sysctl hw.physicalcpu | awk '{print $2}'` \* 2)"
-else
-  local target_jobs="$(nproc)"
-fi
-export BUNDLE_JOBS=$target_jobs
-
-#
 # Python, you make a big mess with your *.pyc bullshit.
 #
 export PYTHONDONTWRITEBYTECODE=1
